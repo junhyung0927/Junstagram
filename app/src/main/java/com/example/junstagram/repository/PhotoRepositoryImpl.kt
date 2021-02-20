3da8ce8dfa31dee4dc8c7311ac40b1fb05498c57
조준형 <whwnsgud22@naver.com>
2021-02-20 16:26:04 +0900

    데이터 셋 바꿈

--- app/src/main/java/com/example/junstagram/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/junstagram/repository/PhotoRepositoryImpl.kt
@@ -13,6 +13,15 @@ class PhotoRepositoryImpl : PhotoRepository {
     override suspend fun getPagingData(page: Int): Pair<List<PhotoPagedList>, Int?> {
         val list = listOf(
             PhotoPagedList("title", R.drawable.sampleimage13, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage1, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage12, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage0, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage3, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage4, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage5, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage6, "bodyText"),
+            PhotoPagedList("title", R.drawable.sampleimage7, "bodyText"),
+
         )
 
         return if (page <= 10) Pair(list, page+1)
