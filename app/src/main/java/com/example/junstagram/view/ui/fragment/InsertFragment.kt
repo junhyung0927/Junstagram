0e8170357ee1b13231a150518bf08c347bdb85a1
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2021-06-01 14:33:39 +0900

    InsertFragment     - 확장 함수 삭제(관련 없는 로직)     - 내부 lastClickedPosition 변수 추가

--- app/src/main/java/com/example/junstagram/view/ui/fragment/InsertFragment.kt
+++ app/src/main/java/com/example/junstagram/view/ui/fragment/InsertFragment.kt
@@ -1,5 +1,6 @@
 package com.example.junstagram.view.ui.fragment
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.view.View
 import com.example.junstagram.R
@@ -7,6 +8,8 @@ import com.example.junstagram.databinding.FragmentInsertBinding
 import com.example.junstagram.view.base.BaseFragment
 import android.content.Intent
 import android.view.ViewTreeObserver
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.GridLayoutManager
@@ -17,10 +20,15 @@ import com.example.junstagram.util.EventObserver
 import com.example.junstagram.util.GalleryPermission
 import com.example.junstagram.view.ui.adapter.GalleryAdapter
 import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.androidx.scope.fragmentScope
 import com.example.junstagram.util.whenReadyDraw as WhenReadyDraw
 
 class InsertFragment : BaseFragment<FragmentInsertBinding>(R.layout.fragment_insert) {
     private val insertViewModel: InsertViewModel by viewModel()
+    private var lastClickedPosition = 0
+    init {
+
+    }
     private val startForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             //binding.photoViewInsertImage.setImageURI(result.data?.data)
@@ -33,20 +41,25 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(R.layout.fragment_ins
         const val KEY_GALLERY_ID_SELECT_IMAGE: String = Intent.EXTRA_MIME_TYPES
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val actionBar: ActionBar?
+
+        
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var actionBar: ActionBar?
 
         binding.lifecycleOwner = this
         binding.apply {
+
             GalleryPermission().requestPermission(context) {
-                val galleryAdapter = GalleryAdapter(requireContext(), insertViewModel, 0)
+                val galleryAdapter = GalleryAdapter(requireContext(), insertViewModel, lastClickedPosition)
                 recyclerViewInsertFragment.apply {
                     layoutManager = GridLayoutManager(requireContext(), 3)
                     setHasFixedSize(true)
                     adapter = galleryAdapter
-                    WhenReadyDraw {
-                        findViewHolderForLayoutPosition(0)?.itemView?.performClick()
-                    }
                 }
             }
         }
